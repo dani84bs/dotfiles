d6dfd0145e6a887864347ccceb4011a0a4152f57
Daniele Trainini <dani84bs@gmail.com>
2022-08-29 16:11:48 +0200

    Update dot_doom.d/init.el

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -153,7 +153,12 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +present +pretty +roam2 +journal +jupyter)               ; organize your plain life in plain text
+       (org
+        +present
+        +pretty
+        +roam2
+        +journal
+        +jupyter)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
