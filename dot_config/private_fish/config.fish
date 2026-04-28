a270472d4f4c0d2fe22d9f765414eb1b0c1e75e8
Daniele Trainini <dani84bs@gmail.com>
2026-04-28 10:23:17 +0200

    feat(fish): distinguish between bat and batcat

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,14 +4,18 @@ if status is-interactive
     atuin init --disable-up-arrow fish | source
 
     alias ls 'lsd '
-    alias cat 'bat '
     alias vim 'nvim '
+    # Handle bat/batcat naming differences across systems
+    if type -q batcat
+        alias cat batcat
+    else if type -q bat
+        alias cat bat
+    end
 end
 
 fish_add_path -g ~/.local/bin
 
 # GhosTTY shell integration
-if set -q TMUX; and test "$TERM_PROGRAM" = "ghostty"
+if set -q TMUX; and test "$TERM_PROGRAM" = ghostty
     set -g fish_handle_reflow 1
 end
-
