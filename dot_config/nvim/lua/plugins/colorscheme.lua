3d5740c799f55740826d53f78fa22b46f0caeaf0
Daniele Trainini <dani84bs@gmail.com>
2026-04-29 11:10:58 +0200

    feat(nvim): use catppuccin

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,26 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- configure catpuccin
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      dim_inactive = {
+        enabled = true, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      },
+    },
+  },
+
+  -- Configure LazyVim to load desired colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-nvim",
+    },
+  },
+}
